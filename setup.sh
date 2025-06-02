4352a2698c529ab2a7e662697e24b96bae9f6022
Paul Scotti <paulpauzle@gmail.com>
2025-06-02 11:07:47 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@
 
 set -e
 
+# Create a new virtual environment and activate it
+python3.13 -m venv foundation_env
+source foundation_env/bin/activate
+
 # Ensure uv is installed for faster package installations
 pip install uv
 
 # Upgrade pip
 pip install --upgrade pip
 
-# Create a new virtual environment and activate it
-python3.11 -m venv foundation_env
-source foundation_env/bin/activate
-
 # Use uv to install packages concurrently
-uv pip install numpy==1.26.4 matplotlib jupyter jupyterlab_nvdashboard jupyterlab ipywidgets scipy tqdm scikit-learn scikit-image accelerate webdataset pandas matplotlib einops ftfy regex h5py wandb nilearn nibabel boto3==1.34.57 open_clip_torch kornia omegaconf decord smart-open ffmpeg-python opencv-python==4.6.0.66 torchmetrics==1.3.0.post0 diffusers==0.23.0 pytorch-lightning==2.0.1 transformers==4.44.2 xformers==0.0.22.post7
+uv pip install numpy==2.2.0 matplotlib jupyter jupyterlab_nvdashboard jupyterlab ipywidgets scipy tqdm scikit-learn scikit-image accelerate webdataset pandas matplotlib einops ftfy regex h5py wandb nilearn nibabel boto3==1.34.57 open_clip_torch kornia omegaconf decord smart-open ffmpeg-python opencv-python==4.6.0.66 torchmetrics==1.3.0.post0 diffusers==0.23.0 pytorch-lightning==2.0.1 transformers==4.44.2 xformers==0.0.22.post7
 uv pip install git+https://github.com/openai/CLIP.git --no-deps
 uv pip install torch==2.4.1 --index-url https://download.pytorch.org/whl/cu121
 uv pip install torchvision==0.19.1
